023b14fa07bdbc314d4be3e8a8abae85da13b5f0
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 15:06:31 +0530

    login and signup axios setup

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import URL from '../url'
 
 const Register = () => {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleRegister = async () => {
+    try{
+      const res = await axios.get(`${URL}/api/auth/signup`,{username, email, password})
+      setUsername(res.data.username)
+      setEmail(res.data.email)
+      setPassword(res.data.password)
+      navigate('/login')
+      setError(false)
+    }
+    catch(err){
+      setError(true)
+      console.log(err)
+    }
+  }
+
   return (
     <>
         <div className='flex flex-row justify-between items-center p-4 bg-black/85 h-20 space-x-4 px-8'>
@@ -11,10 +34,10 @@ const Register = () => {
         <div className="w-full flex justify-center items-center h-[80vh]">
         <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[40%] bg-slate-200 p-12 rounded-xl">
             <h1 className="text-xl font-bold text-left">Create an account</h1>
-            <input className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="text" placeholder="Enter your username" />
-            <input className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="text" placeholder="Enter your email" />
-            <input className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="password" placeholder="Enter your password" />
-            <button className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black ">Register</button>
+            <input onChange={(e)=>setUsername(e.target.value)} className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="text" placeholder="Enter your username" />
+            <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="text" placeholder="Enter your email" />
+            <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 rounded-lg outline-0" type="password" placeholder="Enter your password" />
+            <button onClick={handleRegister} className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black ">Register</button>
             <div className="flex justify-center items-center space-x-3">
                 <p>Already have an account?</p>
                 <p className="text-gray-500 hover:text-black"><Link to="/login">Login</Link></p>
---
+++ src/url.js
@@ -0,0 +1,2 @@
+const URL = 'https://noter-backend-vxy1.onrender.com'
+export default URL
